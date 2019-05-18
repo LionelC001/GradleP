83b19e58b0de18d223bb6baf6936bf486d6c9865
l10nel <l10ne1>
2019-05-18 18:57:11 +0800

    databinding

--- app/src/main/java/com/lionel/gradlepractice/MainActivity.java
+++ app/src/main/java/com/lionel/gradlepractice/MainActivity.java
@@ -1,13 +1,20 @@
 package com.lionel.gradlepractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lionel.gradlepractice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        ((ActivityMainBinding)binding).setGradleText(BuildConfig.build_Type);
+
     }
 }
